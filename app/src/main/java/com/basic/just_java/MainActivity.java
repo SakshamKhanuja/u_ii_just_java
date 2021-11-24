ba1b3da56ee2f1d9b3e2ca8a940a0882c9dcb278
SakshamKhanuja <ssk@outlook.ie>
2021-11-24 11:31:26 +0530

    Part Four 1

--- app/src/main/java/com/basic/just_java/MainActivity.java
+++ app/src/main/java/com/basic/just_java/MainActivity.java
@@ -3,8 +3,12 @@ package com.basic.just_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -18,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
      */
     private int quantity = 2;
 
+    /*
+     * Shows on the go messages to the user.
+     */
+    private Toast toast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,29 +39,73 @@ public class MainActivity extends AppCompatActivity {
      * @param view is the clicked view.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice(quantity);
-        String orderSummary = createOrderSummary(price);
+        EditText customerNameEditText = findViewById(R.id.edit_text_name);
+        String name = customerNameEditText.getText().toString();
+
+        CheckBox whippedCreamCheckBox = findViewById(R.id.checkbox_whipped_cream);
+        boolean addWhippedCream = whippedCreamCheckBox.isChecked();
+
+        CheckBox chocolateCheckBox = findViewById(R.id.checkbox_chocolate);
+        boolean addChocolate = chocolateCheckBox.isChecked();
+
+        int price = calculatePrice(quantity, addWhippedCream, addChocolate);
+
+        String orderSummary = createOrderSummary(price, name, addWhippedCream, addChocolate);
+
+        // Used for Logging.
+        String TAG = "MainActivity";
+
+        Log.v(TAG, "Is Whipped Cream CheckBox Checked? " + addWhippedCream);
+        Log.v(TAG, "Is Chocolate CheckBox Checked? " + addChocolate);
+
         displayMessage(orderSummary);
     }
 
     /**
      * Calculates the total price of the order.
      *
-     * @param quantity is the number of cups ordered.
+     * @param quantity        is the number of cups ordered.
+     * @param addWhippedCream is 'true' if the user has checked the "Whipped Cream" CheckBox,
+     *                        otherwise 'false'.
+     * @param addChocolate    is 'true' if the user has checked the "Chocolate" CheckBox, otherwise
+     *                        'false'.
      * @return the total order amount.
      */
-    private int calculatePrice(int quantity) {
-        return quantity * 30;
+    private int calculatePrice(int quantity, boolean addWhippedCream, boolean addChocolate) {
+        // Represents the base price of a single cup of coffee.
+        int basePrice = 30;
+        // Represents the price to add Whipped Cream topping.
+        int toppingWhippedCream = 10;
+        // Represents the price to add Chocolate topping.
+        int toppingChocolate = 20;
+
+        if (addWhippedCream && addChocolate) {
+            return (basePrice + toppingChocolate + toppingWhippedCream) * quantity;
+        } else if (addWhippedCream) {
+            return (basePrice + toppingWhippedCream) * quantity;
+        } else if (addChocolate) {
+            return (basePrice + toppingChocolate) * quantity;
+        } else {
+            return basePrice * quantity;
+        }
     }
 
     /**
      * Creates the order summary for this order.
      *
-     * @param price is the total price amount.
+     * @param price           is the total price amount.
+     * @param name            is the customer name.
+     * @param addWhippedCream is 'true' if the user has checked the "Whipped Cream" CheckBox,
+     *                        otherwise false.
+     * @param addChocolate    is 'true' if the user has checked the "Chocolate" CheckBox, otherwise
+     *                        false.
      * @return a description of the user order.
      */
-    private String createOrderSummary(int price) {
-        String orderSummary = "Name: Kaptain Kunal";
+    private String createOrderSummary(int price, String name, boolean addWhippedCream,
+                                      boolean addChocolate) {
+        String orderSummary = "Name: " + name;
+        orderSummary += "\nAdd whipped cream? " + addWhippedCream;
+        orderSummary += "\nAdd Chocolate? " + addChocolate;
         orderSummary += "\nQuantity: " + quantity;
         orderSummary += "\nTotal: " + getPriceWithCurrencySymbol(price);
         orderSummary += "\nThank you!";
@@ -65,7 +118,27 @@ public class MainActivity extends AppCompatActivity {
      * @param view is the clicked view.
      */
     public void increment(View view) {
-        displayQuantity(++quantity);
+        if (quantity < 100) {
+            displayQuantity(++quantity);
+        } else {
+            showToast(R.string.toastMoreThan100);
+        }
+    }
+
+    /**
+     * Cancels the ongoing Toast and shows a new Toast.
+     *
+     * @param messageID is a string Resource ID containing the message you want to show.
+     */
+    private void showToast(int messageID) {
+        // If any other toast is showing - remove it.
+        if (toast != null) {
+            toast.cancel();
+        }
+
+        // Send out a new Toast telling customer cannot have less than 1 coffee.
+        toast = Toast.makeText(this, messageID, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     /**
@@ -74,7 +147,11 @@ public class MainActivity extends AppCompatActivity {
      * @param view is the clicked view.
      */
     public void decrement(View view) {
-        displayQuantity(--quantity);
+        if (quantity > 1) {
+            displayQuantity(--quantity);
+        } else {
+            showToast(R.string.toastLessThanOne);
+        }
     }
 
     /**
